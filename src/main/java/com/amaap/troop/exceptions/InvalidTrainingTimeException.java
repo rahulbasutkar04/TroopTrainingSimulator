d2eda928e1a8c60385f78ba60ad0dbe1142ba346
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-25 06:52:56 +0530

    Added excetions classes

---
+++ src/main/java/com/amaap/troop/exceptions/InvalidTrainingTimeException.java
@@ -0,0 +1,7 @@
+package com.amaap.troop.exceptions;
+
+public class InvalidTrainingTimeException extends Exception {
+    public InvalidTrainingTimeException(String message) {
+        super(message);
+    }
+}
