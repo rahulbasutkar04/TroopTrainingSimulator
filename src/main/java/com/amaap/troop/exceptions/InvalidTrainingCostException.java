97551bc426cc3b5f2053ca9ee60fa17ccd8734a1
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-25 06:52:56 +0530

    Added exceptions class
    git commit --amend
    git commit --amend

---
+++ src/main/java/com/amaap/troop/exceptions/InvalidTrainingCostException.java
@@ -0,0 +1,7 @@
+package com.amaap.troop.exceptions;
+
+public class InvalidTrainingCostException extends Exception {
+    public InvalidTrainingCostException(String message) {
+        super(message);
+    }
+}
