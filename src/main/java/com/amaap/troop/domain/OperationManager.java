15624ade9dd5c1cf2f68f70d88241f422ad37814
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-25 06:52:10 +0530

    Updated parameters in manger class

--- src/main/java/com/amaap/troop/domain/OperationManager.java
+++ src/main/java/com/amaap/troop/domain/OperationManager.java
@@ -1,10 +1,12 @@
 package com.amaap.troop.domain;
 
 import com.amaap.troop.exceptions.InvalidQuantityException;
+import com.amaap.troop.exceptions.InvalidTrainingCostException;
+import com.amaap.troop.exceptions.InvalidTrainingTimeException;
 import com.amaap.troop.exceptions.InvalidTroopTypeException;
 
 public class OperationManager {
-    public Troop createTroop(Troopers type, int quantity) throws InvalidQuantityException, InvalidTroopTypeException {
-        return Troop.create(type,quantity);
+    public static Troop createTroop(Troopers type, int quantity, int trainingTime, int trainingCost) throws InvalidQuantityException, InvalidTroopTypeException, InvalidTrainingTimeException, InvalidTrainingCostException {
+        return Troop.create(type, quantity, trainingTime, trainingCost);
     }
 }
