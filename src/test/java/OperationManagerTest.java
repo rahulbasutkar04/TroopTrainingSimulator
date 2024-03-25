ab87c9027592ee26e93597a45e7154b1bb90bfa8
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-25 06:07:57 +0530

    Added Test cases for operation manager

--- src/test/java/OperationManagerTest.java
+++ src/test/java/OperationManagerTest.java
@@ -1,6 +1,28 @@
+import com.amaap.troop.domain.OperationManager;
+import com.amaap.troop.domain.Troop;
+import com.amaap.troop.domain.Troopers;
+import com.amaap.troop.exceptions.InvalidQuantityException;
+import com.amaap.troop.exceptions.InvalidTroopTypeException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OperationManagerTest {
 
+    @Test
+    void shouldAbleToCreateArmyTroopWithTypeBarbarian() throws InvalidQuantityException, InvalidTroopTypeException {
+        // Arrange
+        Troopers type = Troopers.Barbarian;
+        int quantity = 5;
+        Troop expected = Troop.create(type, quantity);
 
+        // Act
+        OperationManager operationManager = new OperationManager();
+        Troop actual = operationManager.createTroop(type, quantity);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 
 
 }
